6a3f9fb850367272e2ed657160eb8243ed548ccb
slowbeam <sedwardscode@gmail.com>
2018-05-26 15:38:55 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  counter = 0
+  until int == 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  counter = 0
+  until int == 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
